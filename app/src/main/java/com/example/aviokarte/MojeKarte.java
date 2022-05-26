5cff841937dd8681cf4179b0ed7565272022d0b1
unknown <vedstandard@runbox.com>
2022-05-26 04:16:25 +0200

    fix

---
+++ app/src/main/java/com/example/aviokarte/MojeKarte.java
@@ -0,0 +1,14 @@
+package com.example.aviokarte;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MojeKarte extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_moje_karte);
+    }
+}
